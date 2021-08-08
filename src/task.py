97d62be01a8f915f650a8c02d1b244e577c4a7b5
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2021-08-08 18:06:41 -0400

    feat: upgrade task to use new base task

--- src/task.py
+++ src/task.py
@@ -1,14 +1,15 @@
 import json
 import os
 
+from task_interfaces import TaskInterface, SubscriptionLevels, TaskTypes
 from code_check import CodeCheck
 from task_interfaces import MetaTaskInterface
 from task_interfaces import SubscriptionLevels
 
 
-class Task(MetaTaskInterface):
+class Task(TaskInterface):
     """
-    Verifies files are formatted with prettier.
+    Runs pylint on files.
     """
 
     name = "Pylint"
@@ -19,21 +20,9 @@ class Task(MetaTaskInterface):
     fail_text = ""
     actions = None
     subscription_level = SubscriptionLevels.FREE
+    type = TaskTypes.CODE_FORMAT
+    source_script_path="%s/task.sh" % os.path.dirname(__file__),
+    handler = "task"
 
     def execute(self, github_body, settings) -> bool:
-        head_branch = github_body["pull_request"]["head"]["ref"]
-        base_branch = github_body["repository"]["default_branch"]
-
-        code_check = CodeCheck(
-            token=github_body.get("githaxs").get("token"),
-            branch=head_branch,
-            default_branch=base_branch,
-            full_repo_name=github_body.get("repository", {}).get("full_name"),
-            source_script_path="%s/task.sh" % os.path.dirname(__file__),
-        )
-
-        code_check.execute()
-
-        self.fail_text = code_check.fail_text
-
-        return code_check.result
+        pass
