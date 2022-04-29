b40af936d8070855745de5fd2b58440b95a2b9ee
Gabe Levasseur <11285350+GabeL7r@users.noreply.github.com>
2022-04-29 11:32:24 -0400

    feat: update task interface (#2)

--- src/task.py
+++ src/task.py
@@ -1,26 +1,14 @@
 import json
 import os
 
-from task_interfaces import TaskInterface, SubscriptionLevels, TaskTypes
+from task_interfaces import StaticAnalysisTask, SubscriptionLevels
 
-class Task(TaskInterface):
+
+class Task(StaticAnalysisTask):
     """
     Runs pylint on files.
     """
 
     name = "Pylint"
-    slug = "pylint"
-    pass_summary = ""
-    pass_text = ""
-    fail_summary = "All files not formatted correctly."
-    fail_text = ""
-    actions = None
-    can_fix = False
     subscription_level = SubscriptionLevels.FREE
-    type = TaskTypes.CODE_FORMAT
-
     source_script_path = "%s/task.sh" % os.path.dirname(__file__)
-    handler = "task"
-
-    def execute(self, github_body, settings) -> bool:
-        pass
